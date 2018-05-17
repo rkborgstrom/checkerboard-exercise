2186b0d2c300296e7cd67074eaf7d1ccd8b642b8
Ryan Borgstrom <Rkborgstrom@mix.wvu.edu>
2018-05-17 16:45:22 -0500

    Step 1

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-// Your JS goes here
+document.addEventListener('DOMContentLoaded', function() {
+let blackSquare = true;
+
+function createCheckerboard () {
+  for (let i = 0; i < 63; i++) {
+    let square = document.createElement("div");
+    square.style.cssText="width: 11.1%; float: left; padding-bottom: 11.1%;";
+    if (blackSquare===false) {
+      square.style.cssText += 'background-color: red;';
+      blackSquare = true; 
+    } else {
+      square.style.cssText += 'background-color: black;';
+      blackSquare = false;
+    }
+    document.body.appendChild(square);
+    console.log(true);
+  }
+}
+createCheckerboard();
+});
