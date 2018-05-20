14cea8b68aac4efe2e3ee1576cfe7bd2cba85c44
Ryan Borgstrom <Rkborgstrom@mix.wvu.edu>
2018-05-20 12:20:07 -0500

    updated checkerboard

--- script.js
+++ script.js
@@ -1,30 +1,27 @@
-document.addEventListener('DOMContentLoaded', function () {
-  let blackSquare = true;
-
+document.addEventListener('DOMContentLoaded', function() {
   const createRandomColors = function() {
     let numberOne = Math.floor(Math.random() * 255);
-    let numberTwo= Math.floor(Math.random() * 255);
-    let numberThree= Math.floor(Math.random() * 255);
-    return (`rgb(${numberOne}, ${numberTwo}, ${numberThree})`); 
- }
-
+    let numberTwo = Math.floor(Math.random() * 255);
+    let numberThree = Math.floor(Math.random() * 255);
+    return (`rgb(${numberOne}, ${numberTwo}, ${numberThree})`);
+  }
+  let number = 0;
+  const numbersInSquares = function() {
+    number++;
+    return number;
+  }
   const createCheckerboard = function() {
     document.body.innerHTML = "";
     for (let i = 0; i < 63; i++) {
       let square = document.createElement("div");
       square.style.cssText = "width: 11.1%; float: left; padding-bottom: 11.1%;";
-      if (blackSquare === false) {
-        square.style.cssText += (`background-color: ${createRandomColors()}`);
-        blackSquare = true;
-      } else {
-        square.style.cssText += `background-color: ${createRandomColors()}`;
-        blackSquare = false;
-      }
+      square.innerHTML = numbersInSquares();
+      square.style.cssText += (`background-color: ${createRandomColors()}`);
       document.body.appendChild(square);
-  
+
     }
   }
-  // createCheckerboard();
-  setInterval(createCheckerboard, 600);
+  createCheckerboard();
+  // setInterval(createCheckerboard, 1200);
 
-}); 
+});
